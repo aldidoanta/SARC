34706effe5e028f1888454d500b8320e70b6a7ac
Aldi Doanta Kurnia <aldidkurnia@gmail.com>
2022-10-28 01:34:19 +0200

    feature: add code to generate plots in .png

--- feature_visualization.py
+++ feature_visualization.py
@@ -6,13 +6,14 @@ def plot_sentiment_features(docs, labels, plot_title, marker):
     compound_previous = [item[0] for item in docs]
     compound_current = [item[1] for item in docs]
 
+    figure = pl.figure(dpi=150)
     pl.scatter(compound_previous, compound_current,
                c=labels, marker=marker,
-               cmap=mpl.colors.ListedColormap(['blue', 'orange']),
+               cmap=mpl.colors.ListedColormap(['black', 'orange']),
                norm=mpl.colors.Normalize(vmin=-1, vmax=1),
                edgecolors='k')
 
-    cm = mpl.colors.ListedColormap(['blue', 'orange'])
+    cm = mpl.colors.ListedColormap(['black', 'orange'])
     pl.legend(loc='lower right',
               handles=[mpl.patches.Patch(facecolor=cm(0.), edgecolor='k', label='Non-sarcastic'),
                        mpl.patches.Patch(facecolor=cm(1.), edgecolor='k', label='Sarcastic')])
@@ -20,4 +21,5 @@ def plot_sentiment_features(docs, labels, plot_title, marker):
     pl.title(plot_title)
     pl.xlabel("Previous sentiment's compound score")
     pl.ylabel("Current sentiment's compound score")
-    pl.show()
+
+    figure.savefig(plot_title + '.png')
