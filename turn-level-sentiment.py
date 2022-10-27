bb69f07eb262993cbb11352ddb11d15cedd83394
Aldi Doanta Kurnia <aldidkurnia@gmail.com>
2022-10-27 15:06:34 +0200

    feature: implement emotion score calculation to complement the sentiment features
    taken from emotion-extraction.py

--- turn-level-sentiment.py
+++ turn-level-sentiment.py
@@ -2,11 +2,12 @@ import nltk
 import numpy as np
 nltk.download('vader_lexicon')
 
-from eval import parse
+from eval import *
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nrclex import NRCLex
 from sklearn.linear_model import LogisticRegressionCV as LogitCV
-from sklearn.metrics import *
-from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics, naive_bayes
+from text_embedding.features import *
 from text_embedding.vectors import *
 from utils import *
 
@@ -60,9 +61,16 @@ def main():
         first_response = train_docs[0][idx]
         second_response = train_docs[1][idx]
 
+        # Calculate sentiment scores
         score_previous_statement = sentiment_analyzer.polarity_scores(previous_statement)
         score_first_response = sentiment_analyzer.polarity_scores(first_response)
         score_second_response = sentiment_analyzer.polarity_scores(second_response)
+
+        # Calculate emotion scores
+        emotion_score_previous_statement = get_emotion_score(get_preprocessed_sentence(previous_statement))
+        # emotion_score_first_response = get_emotion_score(first_response)
+        # emotion_score_second_response = get_emotion_score(second_response)
+
         # Treat the sentiment results (values of neg, neu, pos) as learning features
         train_all_docs_sentiment.append([
             score_previous_statement['neg'],
@@ -70,7 +78,10 @@ def main():
             score_previous_statement['pos'],
             score_first_response['neg'],
             score_first_response['neu'],
-            score_first_response['pos']
+            score_first_response['pos'],
+            # emotion_score_first_response['fear'],
+            # emotion_score_first_response['anger'],
+            # emotion_score_first_response['anticipation'],
         ])
         train_all_docs_sentiment.append([
             score_previous_statement['neg'],
@@ -133,9 +144,33 @@ def main():
     print('\tTest acc: ', (test_pred_labels == test_expect_labels).sum() / test_pred_labels.shape[0])
 
     # Evaluate classifier using NaiveBayes
-    gnb = GaussianNB()
+    gnb = naive_bayes.GaussianNB()
     gnb.fit(train_all_docs_sentiment, train_all_labels)
     y_predict = gnb.predict(test_all_docs_sentiment)
-    print('GaussianNB accuracy: ', accuracy_score(test_all_labels, y_predict))
-    print('GaussianNB f-1 score: ', f1_score(test_all_labels, y_predict))
+    print('GaussianNB accuracy: ', metrics.accuracy_score(test_all_labels, y_predict))
+    print('GaussianNB f-1 score: ', metrics.f1_score(test_all_labels, y_predict))
+
+def get_preprocessed_sentence(sentence):
+    return lemmatize(list(split_on_punctuation(sentence)))
+
+def get_emotion_score(sentence):
+    n_word = len(sentence)
+    emo_count = {'fear': 0, 'anger': 0, 'anticipation': 0, 'trust': 0, 'surprise': 0, 'positive': 0, 'negative': 0, 'sadness': 0, 'disgust': 0, 'joy': 0}
+    for word in sentence:
+        emotion = NRCLex(word).raw_emotion_scores
+        for emo in emotion:
+            emo_count[emo] += 1
+    return {
+        'fear': emo_count['fear']/n_word,
+        'anger': emo_count['anger']/n_word,
+        'anticipation': emo_count['anticipation']/n_word,
+        'trust': emo_count['trust']/n_word,
+        'surprise': emo_count['surprise']/n_word,
+        'positive': emo_count['positive']/n_word,
+        'negative': emo_count['negative']/n_word,
+        'sadness': emo_count['sadness']/n_word,
+        'disgust': emo_count['disgust']/n_word,
+        'joy': emo_count['joy']/n_word
+    }
+
 main()
