406969a2b03e7f4a58870ada6dd182e0598f042e
yesayagm <ygalatia@gmail.com>
2022-10-27 15:09:32 +0200

    added previous sentence to emotion vector

--- emotion-extraction.py
+++ emotion-extraction.py
@@ -21,6 +21,10 @@ def main():
     train_seqs, test_seqs, train_labels, test_labels = \
         load_sarc_responses(train_file, test_file, comment_file, lower=args.lower)
 
+    # Ancestor/prior statements that form the context of the sarcasm statements
+    train_ancestor = train_seqs['ancestors']
+    test_ancestor = test_seqs['ancestors']
+
     # Only use responses for this method. Ignore ancestors.
     train_resp = train_seqs['responses']
     test_resp = test_seqs['responses']
@@ -32,8 +36,13 @@ def main():
     train_labels = {i: [2*int(l[i])-1 for l in train_labels] for i in range(2)}
     test_labels = {i: [2*int(l[i])-1 for l in test_labels] for i in range(2)}
 
+    train_anc_docs = {0: [l[0] for l in train_ancestor]}
+    test_anc_docs = {0: [l[0] for l in test_ancestor]}
+
     # Train a classifier on all responses in training data. We will later use this
     # classifier to determine for every sequence which of the 2 responses is more sarcastic.
+    train_all_ancs_tok = preprocessing(tokenize(train_anc_docs[0]))
+    test_all_ancs_tok = preprocessing(tokenize(test_anc_docs[0]))
     train_all_docs_tok = preprocessing(tokenize(train_docs[0] + train_docs[1]))
     test_all_docs_tok = preprocessing(tokenize(test_docs[0] + test_docs[1]))
     train_all_labels = np.array(train_labels[0] + train_labels[1])
@@ -42,26 +51,37 @@ def main():
     train_all_docs_emo = []
     test_all_docs_emo = []
     # Measuring Emotions in each sentence
-    for sentence in train_all_docs_tok:
-        n_word = len(sentence)
-        emo_count = {"fear": 0, "anger": 0, "anticipation": 0, "trust": 0, "surprise": 0, "positive": 0, "negative": 0, "sadness": 0, "disgust": 0, "joy": 0}
-        for word in sentence:
-            emotion = NRCLex(word).raw_emotion_scores
-            for emo in emotion:
-                emo_count[emo] += 1
-        train_all_docs_emo.append([emo_count["fear"]/n_word, emo_count["anger"]/n_word, emo_count["anticipation"]/n_word, emo_count["trust"]/n_word, emo_count["surprise"]/n_word, emo_count["positive"]/n_word, emo_count["negative"]/n_word, emo_count["sadness"]/n_word, emo_count["disgust"]/n_word, emo_count["joy"]/n_word])
+    for idx, sentence in enumerate(train_ancestor):
+        previous_statement = train_all_ancs_tok[idx]
+        next_statement = preprocessing(tokenize([train_docs[0][idx], train_docs[1][idx]]))
+        first_response = next_statement[0]
+        second_response = next_statement[1]
+
+        emo_prev = extract_emo(previous_statement)
+        emo_first = extract_emo(first_response)
+        emo_second = extract_emo(second_response)
+
+        train_all_docs_emo.append(average_emo(emo_prev, emo_first, len(previous_statement), len(first_response)))
+        train_all_docs_emo.append(average_emo(emo_prev, emo_second, len(previous_statement), len(second_response)))
+
     train_all_docs_emo = np.array(train_all_docs_emo)
 
-    for sentence in test_all_docs_tok:
-        n_word = len(sentence)
-        emo_count = {"fear": 0, "anger": 0, "anticipation": 0, "trust": 0, "surprise": 0, "positive": 0, "negative": 0, "sadness": 0, "disgust": 0, "joy": 0}
-        for word in sentence:
-            emotion = NRCLex(word).raw_emotion_scores
-            for emo in emotion:
-                emo_count[emo] += 1
-        test_all_docs_emo.append([emo_count["fear"]/n_word, emo_count["anger"]/n_word, emo_count["anticipation"]/n_word, emo_count["trust"]/n_word, emo_count["surprise"]/n_word, emo_count["positive"]/n_word, emo_count["negative"]/n_word, emo_count["sadness"]/n_word, emo_count["disgust"]/n_word, emo_count["joy"]/n_word])
+    for idx, sentence in enumerate(test_ancestor):
+        previous_statement = test_all_ancs_tok[idx]
+        next_statement = preprocessing(tokenize([test_docs[0][idx], test_docs[1][idx]]))
+        first_response = next_statement[0]
+        second_response = next_statement[1]
+
+        emo_prev = extract_emo(previous_statement)
+        emo_first = extract_emo(first_response)
+        emo_second = extract_emo(second_response)
+
+        test_all_docs_emo.append(average_emo(emo_prev, emo_first, len(previous_statement), len(first_response)))
+        test_all_docs_emo.append(average_emo(emo_prev, emo_second, len(previous_statement), len(second_response)))
+
     test_all_docs_emo = np.array(test_all_docs_emo)
 
+    print(test_all_docs_emo)
     # Evaluate this classifier on all responses.
     print('Evaluate the classifier on all responses')
     clf = LogitCV(Cs=[10**i for i in range(-2, 3)], fit_intercept=False, cv=2, dual=np.less(*train_all_docs_emo.shape), solver='liblinear', n_jobs=-1, random_state=0)
@@ -84,5 +104,19 @@ def main():
     test_expect_labels = test_labels[0]
     print('\tTrain acc: ', (train_pred_labels == train_expect_labels).sum() / train_pred_labels.shape[0])
     print('\tTest acc: ', (test_pred_labels == test_expect_labels).sum() / test_pred_labels.shape[0])
+
+def extract_emo(sentence):
+    emo_count = {"fear": 0, "anger": 0, "anticipation": 0, "trust": 0, "surprise": 0, "positive": 0, "negative": 0, "sadness": 0, "disgust": 0, "joy": 0}
+    for word in sentence:
+        emotion = NRCLex(word).raw_emotion_scores
+
+        for emo in emotion:
+            if emotion[emo] > 0:
+                emo_count[emo] += 1
+
+    return emo_count
+
+def average_emo(prev_emo, next_emo, len_prev, len_next):
+    return [prev_emo["fear"]/len_prev, prev_emo["anger"]/len_prev, prev_emo["anticipation"]/len_prev, prev_emo["trust"]/len_prev, prev_emo["surprise"]/len_prev, prev_emo["positive"]/len_prev, prev_emo["negative"]/len_prev, prev_emo["sadness"]/len_prev, prev_emo["disgust"]/len_prev, prev_emo["joy"]/len_prev, next_emo["fear"]/len_next, next_emo["anger"]/len_next, next_emo["anticipation"]/len_next, next_emo["trust"]/len_next, next_emo["surprise"]/len_next, next_emo["positive"]/len_next, next_emo["negative"]/len_next, next_emo["sadness"]/len_next, next_emo["disgust"]/len_next, next_emo["joy"]/len_next]
 
 main()
